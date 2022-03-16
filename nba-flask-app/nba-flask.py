ccec3d16a684f3ba5d5a58be325e783fdef375b1
Zach Teutsch <88554871+zateutsch@users.noreply.github.com>
2022-03-16 14:53:38 -0700

    Turning flask portion into API instead of website, UI is still served on /ui/nba/livespread

--- nba-flask-app/nba-flask.py
+++ nba-flask-app/nba-flask.py
@@ -1,10 +1,31 @@
-from flask import Flask, render_template, request
-import src.livebettingmachine as BettingMachine, os
+from flask import Flask, render_template, request, jsonify
+import src.livebettingmachine as BettingMachine
+import os
+
 app = Flask(__name__)
 
+@app.route('/api')
+def routes():
+    routes = {
+        "/api/nba/livespread": ["quarter", "time", "lead", "livespread", "openspread"]
+    }
 
+    return jsonify(routes)
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/api/nba/livespread')
+def nbaLivespread():
+    params = request.args
+    quarter = int(params['quarter'])
+    time = params['time']
+    lead = float(params['lead'])
+    livespread = float(params['livespread'])
+    openspread = float(params['openspread'])
+
+    p, r = BettingMachine.LiveBetSpread(quarter, time, lead, livespread, openspread)
+
+    return jsonify({"probability": p, "count": len(r), "results": r})
+
+@app.route('/ui/nba/livespread', methods=['POST', 'GET'])
 def home():
     
     game_data = {
@@ -37,8 +58,8 @@ def home():
 
         print(bet_result)
         
-    return render_template("index.html", data=game_data, result=bet_result, count=len(results))
+    return render_template("nba-livespread.html", data=game_data, result=bet_result, count=len(results))
 
 if __name__ == '__main__':
-    app.run(host=os.getenv('IP', '0.0.0.0'), 
-        port=int(os.getenv('PORT', 4444)))
+    app.run(host=os.getenv('IP', '0.0.0.0'),  
+        port=int(os.getenv('PORT', 4444))) 
